e20b694761ef40578e1b19b4d857ee99649e1a1b
Raiondesu <a.ishakov@innopolis.ru>
2016-07-09 00:03:22 +0300

    MOOORE CHANGES

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,11 @@
 
 var app = new Marionette.Application();
 
+var token;
+
 app.addRegions({
-	mainRegion: "#wrapper" // Указываем куда будет рендериться приложение
+	mainRegion: "#wrapper",
+	headerRegion: "#main_header"
 });
 
 
@@ -13,8 +16,7 @@ LoginFormModel = Backbone.Model.extend({
 	// Поля модели, доступные по умолчанию(в т.ч. если в конструктор модели не будут
 	//	переданы какие-то из параметров, )
 	defaults: {
-		username: '',
-		password: ''
+		
 		// Здесь могут быть и любые другие поля если необходимо
 	}
 });
@@ -25,9 +27,75 @@ LoginFormModel = Backbone.Model.extend({
 
 function formSuccess (result) {
 	$('#wrapper').find('.tooltip').removeClass('show');
-
 	$.cookie('usertoken', result.token);
-	location.reload();
+
+	$('#content').addClass('hidden');
+	$('body').addClass('trans');
+	$('body').addClass('logged-in');
+
+	setTimeout(function () {
+		app.mainRegion.empty();
+		app.mainRegion.show(new DashboardView(new DashboardModel()));
+		//app.headerRegion.show(new HeaderView(new HeaderModel()));
+	}, 200);
+
+}
+
+function callback (user_info) {
+
+	$('#content').removeClass('hidden');
+
+	token = $.cookie('usertoken');
+
+
+	if (user_info.lastName)
+		$('#name').append(user_info.lastName);
+	if(user_info.firstName) 
+		$('#name').append(' ' + user_info.firstName);
+	if(user_info.patronymic)
+		$('#name').append(' ' + user_info.patronymic);
+
+	if (user_info.tgId)
+		$('#nickname').append(user_info.tgId);
+	else
+		$("#nickname").append(user_info.username);
+
+	$('#role').append(user_info.role);
+
+	//if (user_info.role == 'student')
+		//$('#content').append('<button class="rounded blue btn">Rule your INNOPOINTS</button>');
+
+	if (user_info.role == 'moderator')
+		listAccounts(token,
+			function (result) {
+				var table_header = '<div class="tbl-header"><table cellpadding="0" cellspacing="0" border="0"><thead><tr><th>#</th><th>Username</th><th>Name</th><th>Role</th></tr></thead></table></div>';
+
+				var table_start = '<div class="tbl-content"><table cellpadding="0" cellspacing="0" border="0"><thead>';
+				var table_end   = '</thead></table></div>';
+
+				var table = table_header + table_start;
+
+				result.forEach( 
+					function(element, index) {
+						table = table + '<tr><td>' + index + '</td><td>' + element.username + '</td><td>' + element.firstName + '</td><td><select data="'+element.id+'"><option ' + (element.role == 'ghost' ? 'selected' : '') + ' value="0">ghost</option><option ' + (element.role == 'student' ? 'selected' : '') + ' value="1">student</option></select></td></tr>';
+					}
+				);
+				$('select').change(function () {
+					var id = this.getAttribute("data");
+					console.log(this.value);
+					var role = this.value == '0' ? 'ghost' : 'student';
+					updateRole(token, id, role);
+					listAccounts(token);
+				});
+
+				var table = table + table_end;
+
+				$('#content').append(table);
+			}
+		);
+
+	
+	$('.header-two-bars').addClass('show');
 }
 
 function formError (error) {
@@ -45,10 +113,14 @@ LoginFormView = Marionette.ItemView.extend({
 		// Принимаем инстанс модели, к которой будет привязана вьюшка
 		this.model = model;
 	},
+
 	// Имя html-шаблона, с которым связывается вьюшка
-	template: '#login_form_template',
-
-
+	getTemplate: function () {
+		if (!$.cookie('usertoken'))
+			return '#login_form_template';
+		else
+			return '#dashboard_template';
+	},
 
 	// События, на которые будет реагировать вьюшка
 	events: {
@@ -76,71 +148,68 @@ LoginFormView = Marionette.ItemView.extend({
 	}
 });
 
-function callback (user_info) {
-
-		$('#content').css('max-width', '700px');
-		$('#content').css('min-width', '400px');
-
-		var token = $.cookie('usertoken');
-
-		$("#username-field").append(user_info.username);
-
-		if (user_info.lastName)
-			$('#name').append(user_info.lastName);
-		if(user_info.firstName) 
-			$('#name').append(' ' + user_info.firstName);
-		if(user_info.patronymic)
-			$('#name').append(' ' + user_info.patronymic);
-
-		if (user_info.tgId)
-			$('#tgId').append(user_info.tgId);
-
-		$('#role').append(user_info.role);
-
-		if (user_info.role == 'student')
-			$('#content').append('<button class="rounded blue btn">Rule your INNOPOINTS</button>');
-
-
-		listAccounts(token,
-			function (result) {
-				result.forEach( 
-					function(element, index) {
-						$('#content').append('<p id="user-' + index + '">User #' + index + ': ' + element.username + ' . His role is ' + element.role + ';');
-						$('#content').append(' Change to <select data="'+element.id+'"><option ' + (element.role == 'ghost' ? 'selected' : '') + ' value="0">ghost</option><option ' + (element.role == 'student' ? 'selected' : '') + ' value="1">student</option></select></p><br/>');
-					}
-				);
-				$('select').change(function () {
-					var id = this.getAttribute("data");
-					var role = $('this').val() == 0 ? 'ghost' : 'student';
-					updateRole(token, id, role);
-					listAccounts(token);
-				});
-			}
-		);
-}
-
-
-LogoutFormModel = Backbone.Model.extend({
+HeaderModel = Backbone.Model.extend({
 	//
 });
 
-LogoutFormView = Marionette.ItemView.extend({
+HeaderView = Marionette.ItemView.extend({
+	initialize: function (model) {
+		this.model = model;
+	},
+
+	template: '#header_template',
+
+	events: {
+		'click #logout_button': function () {
+			$('.header-two-bars').removeClass('show');
+			$('body').removeClass('logged-in');
+
+			$('#content').addClass('hidden');
+
+			setTimeout(function () {
+				$.removeCookie('usertoken');
+
+				app.mainRegion.empty();
+				app.mainRegion.show(new LoginFormView(new LoginFormModel()));
+				app.headerRegion.empty();
+
+				setTimeout(function() {$('body').removeClass('trans');}, 500);
+
+				$('#content').removeClass('hidden');
+			}, 500);
+
+		}
+	}
+});
+
+DashboardModel = Backbone.Model.extend({
+	//
+});
+
+DashboardView = Marionette.ItemView.extend({
 	initialize: function(model) {
 		// Это конструктор вьюшки
 		// Принимаем инстанс модели, к которой будет привязана вьюшка
 		this.model = model;
 
-		var token = $.cookie('usertoken');
-		getAccount(token, callback);
+		token = $.cookie('usertoken');
+		if (token) {
+			app.headerRegion.show(new HeaderView(new HeaderModel()));
+			$('body').addClass('trans');
+			$('body').addClass('logged-in');
+			getAccount(token, callback);
+		}
+		else {
+			app.mainRegion.empty();
+			app.mainRegion.show(new LoginFormView(new LoginFormModel()));
+			app.headerRegion.empty();
+		}
 	},
 	// Имя html-шаблона, с которым связывается вьюшка
-	template: '#personal_page_template',
+	template: '#dashboard_template',
 	// События, на которые будет реагировать вьюшка
 	events: {
-		'click #logout_button': function (argument) {
-			$.removeCookie('usertoken');
-			location.reload();
-		}
+		
 	}
 });
 
@@ -158,17 +227,15 @@ app.addInitializer(function(options) {
 
 
 	// Создаём инстанс модели, который привяжем ко вьюхе
-	var logoutFormModel = new LogoutFormModel();
+	var dashboardModel = new LoginFormModel();
 	// Создаём инстанс вьюхи, который будет отображаться; передаём туда модель
-	var logoutFormView = new LogoutFormView(logoutFormModel);
+	var dashboardView = new DashboardView(dashboardModel);
+
+	var headerView = new HeaderView(new HeaderModel);
 
 	// Рендерим нашу вьюху в один из регионов приложения, определённый нами в самом начале
 
-	if (!$.cookie('usertoken'))
-		app.mainRegion.show(loginFormView);
-	else 
-		/*TODO Redirect to personal page by user's token.*/
-		app.mainRegion.show(logoutFormView);
+	app.mainRegion.show(loginFormView);
 });
 
 
