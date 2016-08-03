c3139e673fec3316afd182c3e59a1706b5408dd8
Raiondesu <a.ishakov@innopolis.ru>
2016-08-03 03:23:08 +0300

    Added user's role label.

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,9 +1,15 @@
 <template>
-	<button v-link="'/'"			title="">main</button>
-	<button v-link="'/test'"		title="">test</button>
-	<button v-link="'/innopoints'"	title="">innopoints</button>
-	<button v-link="'/admin_side'"	v-if="user.isAdmin"	title="">admin_side</button>
-	<button @click="logout" 		title="">logout</button>
+	<div block>
+		<span>{{ user.role | capitalize }}'s dashboard</span>
+	</div>
+	<div block>
+		<button v-link="'/'"			title="">main</button>
+		<button v-link="'/test'"		title="">test</button>
+		<button v-link="'/innopoints'"	title="">innopoints</button>
+		<button v-link="'/profile'"		title="">my profile</button>
+		<button v-link="'/admin_side'"	v-if="user.isAdmin"	title="">admin_side</button>
+		<button @click="logout" 		title="">logout</button>
+	</div>
 </template>
 
 <script>
