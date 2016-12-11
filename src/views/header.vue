bf7bbf9f4e59099aa5a92e04d716b0ffc3637b9d
Bulat <bulnab@gmail.com>
2016-12-11 21:13:22 +0300

    First iteration of orders page

--- src/views/header.vue
+++ src/views/header.vue
@@ -1,20 +1,18 @@
 <template lang="jade">
 	header.header-main.clearfix
-		.hidden-md-up.sidebar-show
-			button.btn.btn-outline-secondary()
-				span {{{':hamburger:' | emojify}}}
+		// .hidden-md-up.sidebar-show
+		// 	button.btn.btn-outline-secondary()
+		// 		span {{{':hamburger:' | emojify}}}
 		slot
 </template>
 
 <script>
-	module.exports = {
-		data: function () {
+	export default {
+		
+		data() {
 			return {
 				
 			}
 		},
-		methods: {
-			
-		}
 	}
 </script>
--- src/views/store/orders.vue
+++ src/views/store/orders.vue
@@ -1,7 +1,40 @@
 <template lang="jade">
-	.container-fluid
-		.card-columns
-			
+	.container
+			.card.card-block(v-for="o in orders" status="{{ o.status }}", id="card-{{ o.id }}")
+				span.float-xs-right {{ o.creation_date }}
+				h2.card-title.clearfix
+					span.tag.tag-default.tag-success.float-xs-left(status="{{ o.status }}") {{ o.status.split('_').join(' ') | capitalize }}
+					span.float-xs-left.mx-1 {{(o._id = '#' + o.id)}} {{ o.type | capitalize }}
+						small.text-muted  by {{o.author.username}}
+				h4
+					template(v-if="o.items > 0") Items
+					template(v-else) Item
+				.table-responsive
+					table.table.table-striped.table-bordered.table-sm
+						thead
+							tr
+								th.text-xs-center #
+								th.text-xs-center Title
+								th.text-xs-center Amount
+								th.text-xs-center Category
+								th.text-xs-center Porperties
+								th.text-xs-center Price
+						tbody
+							tr(v-for="i in o.items")
+								th(scope='row') {{ $index + 1 }}
+								// TODO: add a link to item page
+								td {{ i.title }}
+								td {{  i.amount }}
+								td {{  i.category.title }}
+								td 
+									template(v-if="i.properties")
+										template(v-for="(key, value) in i.properties")
+											p.mb-0
+												span {{ key }}:
+												span.font-weight-bold  {{ value }}
+									template(v-else) -
+								td {{ i.price }}
+				// pre {{ o | json }}
 </template>
 
 <script>
@@ -19,15 +52,21 @@
 		
 		route: {
 			data(transition) {
-				this.$router.app.user.innopoints.api.user.getListOfOrders({
-					successCallback: orders => {
+				this.$router.app.user.innopoints.api.user.getListOfOrders(
+					(orders) => {
+						console.log(orders)
+
+						orders.forEach((o) => {
+							o.creation_time = o.creation_date;
+							o.creation_date = new Date(o.creation_time * 1000).toLocaleString('ru');
+						})
+
 						transition.next({
 							orders,
-						});
-						console.log(orders)
+						})
 					},
-					errorCallback: console.log,
-				})
+					console.log
+				)
 			} 
 		},
 	}
